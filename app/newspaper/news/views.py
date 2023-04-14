dc7e2bf240e33d65870c267f523eb62b618338d7
Ruslan-Gabbazov <ruslan.gabbazov01@gmail.com>
2023-04-14 08:29:34 +0300

    hello world done

--- app/newspaper/news/views.py
+++ app/newspaper/news/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def news_list(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('<h1>Hello world</h1>')
