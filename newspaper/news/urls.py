e2ba0f7e7f5ad9acdb9a5115f147f75149b83b78
Ruslan-Gabbazov <ruslan.gabbazov01@gmail.com>
2023-04-14 17:02:22 +0300

    news card done

--- newspaper/news/urls.py
+++ newspaper/news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', news_list)
+    path('', news_list, name='news_list_url'),
+    path('news/<str:slug>/', news_content, name='news_content_url')
 ]
--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from .models import News
+
 
 # Create your views here.
 
-def news_list(request: HttpRequest):
-    variables = [42 for _ in range(10)]
-    return render(request, 'news/index.html', context={'variables': variables})
+def news_list(request: HttpRequest) -> HttpResponse:
+    news = News.objects.all()
+    return render(request, 'news/index.html', context={'news': news})
+
+
+def news_content(request: HttpRequest, slug: str) -> HttpResponse:
+    news = News.objects.get(slug__iexact=slug)
+    return render(request, 'news/news_content.html', context={'news': news})
+
